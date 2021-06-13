d20bd1544ae16689bbda3778b19676065621ef78
Sniphs98 <Lukas@bronstering.com>
2021-06-13 18:55:08 +0200

    test

--- piStarty.py
+++ piStarty.py
@@ -26,12 +26,12 @@ def print_hi():
 
 
 def yes_or_no(question):
-    while "the answer is invalid":
-        reply = str(input(question + ' (y/n): ')).lower().strip()
-        if reply[0] == 'y':
-            return True
-        if reply[0] == 'n':
-            return False
+
+    reply = str(input(question + ' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
 
 
 if __name__ == '__main__':
